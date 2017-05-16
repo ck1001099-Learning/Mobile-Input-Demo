f2a3f66624821b4cb3d3ca77542ed25de4beff0e
ck1001099 <b03202042@ntu.edu.tw>
2017-05-16 12:54:57 +0800

    Mobile Input Demo ver Game Design

---
+++ Assets/Scripts/CharacterTransform.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+
+public class CharacterTransform : MonoBehaviour {
+
+	public TransformGesture transformGesture;
+
+	// Use this for initialization
+	void Start () {
+
+		transformGesture.TransformStarted += (object sender, System.EventArgs e) => 
+		{
+		};
+
+		transformGesture.Transformed += (object sender, System.EventArgs e) => 
+		{
+			this.transform.localScale *= transformGesture.DeltaScale;
+		};
+
+		transformGesture.TransformCompleted += (object sender, System.EventArgs e) => 
+		{
+		};
+	}
+}
---
+++ Assets/Scripts/platformGestureManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+using TouchScript.Hit;
+using DG.Tweening;
+
+using UnityEngine.UI;
+
+
+public class platformGestureManager : MonoBehaviour {
+
+	public TapGesture singleTap;
+	public TapGesture doubleTap;
+
+	public FlickGesture flick;
+
+	public GameObject character1;
+	public GameObject character2;
+	public GameObject nowCharacter;
+	public bool switchCharacter;
+
+	// Use this for initialization
+	void Start () {
+
+		switchCharacter = false;
+		nowCharacter = character1;
+
+		singleTap.Tapped += (object sender, System.EventArgs e) => 
+		{
+			TouchHit hit;
+			singleTap.GetTargetHitResult(out hit);
+
+			nowCharacter.GetComponent<Animator>().SetTrigger("OneTap");
+		};
+
+		doubleTap.Tapped += (object sender, System.EventArgs e) => 
+		{
+			TouchHit hit;
+			doubleTap.GetTargetHitResult(out hit);
+
+			nowCharacter.GetComponent<Animator>().SetTrigger("DoubleTap");
+		};
+
+		flick.Flicked += (object sender, System.EventArgs e) => {
+			ChangeCharacter();
+		};
+
+	}
+
+	void Update(){
+		if (Input.acceleration.magnitude > 1.7f) {
+			nowCharacter.GetComponent<Animator> ().SetTrigger ("Shake");
+		}
+	}
+
+	void ChangeCharacter(){
+		if (switchCharacter) {
+			switchCharacter = false;
+			character1.gameObject.SetActive (true);
+			character2.gameObject.SetActive (false);
+			nowCharacter = character1.gameObject;
+		} else {
+			switchCharacter = true;
+			character1.gameObject.SetActive (false);
+			character2.gameObject.SetActive (true);
+			nowCharacter = character2.gameObject;
+		}
+	}
+}
